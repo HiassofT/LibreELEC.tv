e4acb43da70600441f4bc0e4a2f779d4ab128d1c
Matthias Reichl <hias@horus.com>
2023-09-17 12:10:05 +0200

    TEST inputstream.adaptive: update to latest version
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="21.2.1-Omega"
-PKG_SHA256="136ebdd1836a68fb586cc3f4e688ad926342b6fde5691aaf299b8f56c6da263c"
-PKG_REV="1"
+PKG_VERSION="ec36bf105a14a037d1169d57f10ae796817d78e9"
+#PKG_SHA256="136ebdd1836a68fb586cc3f4e688ad926342b6fde5691aaf299b8f56c6da263c"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/inputstream.adaptive"
@@ -19,6 +19,8 @@ PKG_IS_ADDON="yes"
 addon() {
   install_binary_addon ${PKG_ADDON_ID}
 
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
-  cp -P ${PKG_BUILD}/.${TARGET_NAME}/wvdecrypter/libssd_wv.so ${ADDON_BUILD}/${PKG_ADDON_ID}
+  if [ "${ARCH}" = "aarch64" ]; then
+    mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+    cp -P ${PKG_BUILD}/.${TARGET_NAME}/lib/cdm_aarch64/libcdm_aarch64_loader.so ${ADDON_BUILD}/${PKG_ADDON_ID}
+  fi
 }
