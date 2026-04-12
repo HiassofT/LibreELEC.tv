91dc5221e3ea808b10026669b8977280ab7e0126
Matthias Reichl <hias@horus.com>
2026-04-12 14:18:43 +0200

    cage: depend on kanshi and add helper scripts to start a wayland session
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/wayland/compositor/cage/package.mk
+++ packages/wayland/compositor/cage/package.mk
@@ -7,7 +7,15 @@ PKG_SHA256="acab0c83175164a788d7b9f89338cbdebdc4f7197aff6fdc267c32f7181234a9"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.hjdskes.nl/projects/cage"
 PKG_URL="https://github.com/cage-kiosk/cage/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain wayland wayland-protocols libxkbcommon wlroots"
+PKG_DEPENDS_TARGET="toolchain wayland wayland-protocols libxkbcommon wlroots kanshi"
 PKG_LONGDESC="A Wayland kiosk "
 
 PKG_MESON_OPTS_TARGET="-Dman-pages=disabled"
+
+post_makeinstall_target() {
+  mkdir -p "${INSTALL}/usr/bin"
+    cp "${PKG_DIR}/scripts/start-wayland-session" "${INSTALL}/usr/bin"
+
+  mkdir -p "${INSTALL}/usr/lib/libreelec"
+    cp "${PKG_DIR}/scripts/wayland-session-helper" "${INSTALL}/usr/lib/libreelec"
+}
